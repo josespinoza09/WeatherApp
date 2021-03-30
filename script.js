1580b76baa88df4b8273d2fe7e821b4d793b1c11
Jose Espinoza <jose20019004@gmail.com>
2021-03-30 18:39:21 -0400

    fixed local storage issues

--- script.js
+++ script.js
@@ -1,100 +1,123 @@
-//Required Variables
-var searchData
-var searchDataForecast
-var uvValue
+const apiKey = '88547ae129179c5b0317315ddf40567c';
 
-//Dates 
-var date = moment().format("MMM Do YY");
-
-
-let savedCities = localStorage.savedCities ? JSON.parse(localStorage.savedCities) : [];
-
-if (savedCities.length > 0) {
-  for (let i = 0; i < savedCities.length; i++) {
-    document.querySelector('#cityList').innerHTML +=
-`<li><button type="button" class="btn btn-light" style="border-color:black" onClick=getApi('${savedCities[i]}')>${savedCities[i]}</li>`
-  }
-}
-//get search and display
-async function getSearch() {
-  // Clears search before running
-  document.querySelector('#currentWeather').innerHTML = "";
-  document.querySelector('#forecast').innerHTML = "";
-  //fetch search input
-  var search = document.querySelector('#search').value
-  search = search[0].toUpperCase() + search.substring(1);
-  //Api request
-  getApi(search)
-  //check if city already exist in list
-  let exist = false;
-  for (let i = 0; i < savedCities.length; i++) {
-    if (search === savedCities[i]) {
-      exist = true;
-      break;
+// Load in saved locations
+let cityList = localStorage.cityList ? JSON.parse(localStorage.cityList): [];
+if (cityList.length > 0) {
+    document.querySelector('#searchRow').classList.remove('d-none');
+    document.querySelector('#resetRow').classList.remove('d-none');
+    for (let i=0; i<cityList.length; i++) {
+        document.querySelector('#searchList').innerHTML +=
+        `<li><button type="button" class="btn btn-link" onclick=queryAPI('${cityList[i]}')>${cityList[i]}</button></li>`;
     }
-  }
-
-  //adds cities to saved list 
-  if (!exist) {
-    savedCities.push(search)
-    document.querySelector('#cityList').innerHTML += `
-    <li><button type="button" class="btn btn-light" style="border-color:black" onClick=getApi('${search}')>${search}</li>`
-    localStorage.savedCities = JSON.stringify(savedCities)
-  }
-}
-async function getApi(city) {
-  //Clears search before adding new cards
-  document.querySelector('#currentWeather').innerHTML = "";
-  document.querySelector('#forecast').innerHTML = ""
-  // url's needed for the api's
-  var searchUrl = ` http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a40946fd9d2010f2fa6e5550cbf15f24&units=metric`
-  var searchForecast = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=a40946fd9d2010f2fa6e5550cbf15f24&units=metric`
-  //fetch info from api
-  searchData = await fetch(searchUrl).then(r => r.json())
-  searchDataForecast = await fetch(searchForecast).then(r => r.json())
-  var searchUv = `http://api.openweathermap.org/data/2.5/uvi?lat=${searchData.coord.lat}&lon=${searchData.coord.lon}&appid=a40946fd9d2010f2fa6e5550cbf15f24`
-  uvValue = await fetch(searchUv).then(r => r.json())
-  displayData(searchData, searchDataForecast, uvValue)
 }
 
-function displayData(weatherInfo, weatherforecast, uv) {
-  //checks uv conditions
-  let color = ""
-  if (uvValue.value <= 2) {
-    color = "success"
-  } else if (uvValue.value <= 7) {
-    color = "warning"
-  } else {
-    color = "danger"
-  }
-  // Weather Icon
-  var iconUrl = `http://openweathermap.org/img/w/${weatherInfo.weather.icon}.png`
-  //Display cards
-  document.querySelector('#currentWeather').innerHTML += `
+// Bind functions to HTML
+document.querySelector('#searchVar').addEventListener('keypress', (e) => onKeyPress(e));
+document.querySelector('#resetList').addEventListener('click', resetList);
+
+// search bar search
+async function onKeyPress(e) {
+    if (e.key === 'Enter') {
+        let searchVar = document.querySelector('#searchVar').value;
+        searchVar = searchVar[0].toUpperCase() + searchVar.substring(1);
+        // display history
+        document.querySelector('#searchRow').classList.remove('d-none');
+        document.querySelector('#resetRow').classList.remove('d-none');
+        // send API request
+        queryAPI(searchVar);
+        // check if entry already in past search list
+        let entryExist = false;
+        for (let i=0; i<cityList.length; i++) {
+            if (searchVar === cityList[i]) {
+                entryExist = true;
+                break;
+            }
+        }
+        // save to list
+        if (!entryExist) {
+            cityList.push(searchVar);
+            document.querySelector('#searchList').innerHTML +=
+            `<li><button type="button" class="btn btn-link" onclick=queryAPI('${searchVar}')>${searchVar}</button></li>`;
+            localStorage.cityList = JSON.stringify(cityList);
+        }
+    }
+}
+
+async function queryAPI(city) {
+    // send API request
+    let lat='', lon='';
+    let errCaught = false, errCode='', errMessage='';
+    let weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    .then( r => r.json());
+    if (weather.cod == 200) {
+        handleWeather(weather);
+        lat = weather.coord.lat;
+        lon = weather.coord.lon;
+    }
+    else {
+        errCode = weather.cod;
+        errMessage = weather.message;
+        errCaught = true;
+    }
+    let uvInfo = await fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    .then( r => r.json()).catch(() => console.error('UV Info Error'));
+    if (uvInfo) handleUV(uvInfo);
+    let forecast = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+    .then( r => r.json())
+    if (forecast.cod == 200) handleForecast(forecast);
+    else {
+        errCode = forecast.cod;
+        errMessage = forecast.message;
+        errCaught = true;
+    }
+    // Handle error
+    if (errCaught) alert(`Error ${errCode}: ${errMessage}`);
+}
+
+function handleWeather(obj) {
+    // print to HTML
+    document.querySelector('#cityName').innerHTML = `${obj.name}, ${obj.sys.country}'s `;
+    document.querySelector('#weatherInfo').innerHTML =
+    `<li>Weather: ${obj.weather[0].main}</li>
+    <li>Temperature: ${(obj.main.temp-273.15).toFixed(2)}&#176;C</li>
+    <li>Humidity: ${obj.main.humidity}%</li>
+    <li>Pressure: ${obj.main.pressure/10} kPa</li>
+    <li>Wind Speed: ${obj.wind.speed} m/s</li>`;
+    document.querySelector('#weatherIcon').innerHTML =
+    `<img src="https://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png" alt="weather icon" />`;
+}
+
+function handleUV(uvInfo) {
+    let output, color;
+    if (uvInfo.value < 2.5) {output = 'low'; color='green';}
+    else if (uvInfo.value < 5.5) {output = 'moderate'; color='orange';}
+    else if (uvInfo.value < 8) {output = 'high'; color='red';}
+    else {output='very high'; color='purple';}
+    document.querySelector('#weatherInfo').innerHTML +=
+    `<li>UV Index: <b style='color:${color}'>${output}</b></li>`;
+}
+
+function handleForecast(obj) {
+    // print to HTML
+    document.querySelector('#forecastHead').classList.remove('d-none');
+    document.querySelector('#forecastInfo').innerHTML = '';
+    for (let i=4; i<37; i+=8) {
+        document.querySelector('#forecastInfo').innerHTML +=
+        `<div class="col-6 col-md-4">
             <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">${weatherInfo.name} ${date} <img id='weatherIcon' src="http://openweathermap.org/img/w/${weatherInfo.weather[0].icon}.png"> </h5>
-              <p class="card-text">Temperature: ${weatherInfo.main.temp}°C</p>
-              <p class="card-text">Humidity: ${weatherInfo.main.humidity}%</p>
-              <p class="card-text">Wind Speed: ${weatherInfo.wind.speed}MPH</p>
-              <p class="card-text" style="width:15% ; background-color:">UV Index: <button type="button" class="btn btn-${color}">${uv.value}</button></p>
+                <div class="card-body">
+                    <h5 class="card-title">${obj.list[i].dt_txt.split(" ",1)}</h5>
+                    <img src="https://openweathermap.org/img/wn/${obj.list[i].weather[0].icon}@2x.png" alt="weather icon"/>
+                    <p class="card-text">${obj.list[i].weather[0].main}</p>
+                    <span class="card-text">Temperature: ${(obj.list[i].main.temp-273.15).toFixed(2)}&#176;C</span>
+                    <span class="card-text">Humidity: ${obj.list[i].main.humidity}%</span>
+                </div>
             </div>
-          </div>`
-  document.querySelector('#forecast').innerHTML += `<div class="col"><h3>5-Day Forecast:</h3></div>`
-  for (var i = 0; i < 5; i++) {
-    date = moment().add(i + 1, 'days').format("MMM Do YY");
-    document.querySelector('#forecast').innerHTML += `
-            <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">${date}</h5>
-              <img id='weatherIcon' src="http://openweathermap.org/img/w/${weatherforecast.list[i].weather[0].icon}.png">
-              <p class="card-text">Temp: ${weatherforecast.list[i].main.temp}°C</p>
-              <p class="card-text">Humidity: ${weatherforecast.list[i].main.humidity}%</p>
-            </div>
-          </div>`
- }
-  }
-    function clearHistory() {
-    localStorage.removeItem('savedCities');
-     document.querySelector('#cityList').innerHTML = "";
+        </div>`;
+    }
+}
+
+function resetList() {
+    localStorage.removeItem('cityList');
+    document.querySelector('#searchList').innerHTML = '';
 }
